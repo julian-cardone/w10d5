6e16cd4d899e7e571e950eb6f1b4ad6dda55db08
Julian Cardone <juliancardone1@gmail.com>
2022-11-18 14:31:57 -0500

    phase 1

--- src/components/GalleryNavigation/index.jsx
+++ src/components/GalleryNavigation/index.jsx
@@ -1,7 +1,18 @@
-function GalleryNavigation() {
+import harvardArt from "../../data/harvardArt";
+import { NavLink } from "react-router-dom";
+
+function GalleryNavigation({galleries}) {
+  // console.log(galleries);
   return (
+  <nav>
+    <NavLink to="/">
     <h1>Galleries</h1>
+    </NavLink>
+  </nav>
+
+  
   )
 }
 
 export default GalleryNavigation;
+
